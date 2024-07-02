e09e8186b08f5dd767994f2ce0721ea5a39846a7
Waterzooi <edrupy@gmail.com>
2024-07-02 08:44:36 +0200

    random select which key to press twice

--- KeepFromIdling.py
+++ KeepFromIdling.py
@@ -1,17 +1,24 @@
 #! python3
 
 import time
+import random
 import pyautogui
 
-WaitTime = 3*59
-
 def Execute():
+    keys = ['numlock', 'capslock', 'shift']
+    lastUsedKey = None
     while True:
-        print('press numlock x2 in 1 sec')
+        availableKeys = [key for key in keys if key != lastUsedKey]
+        key = random.choice(availableKeys)
+        lastUsedKey = key
+        waitTime = random.randint(50, 150)
+        print(f'press {key:<8} x2 in 1 sec - sleep {waitTime:<3} sec afterwards')  #:<8 ensures left-aligned within an 8-character wide field
         time.sleep(1)
-        pyautogui.hotkey('numlock')
-        pyautogui.hotkey('numlock')
-        time.sleep(WaitTime)
+        pyautogui.hotkey(key)
+        time.sleep(0.01)
+        pyautogui.hotkey(key)
+        time.sleep(waitTime)
+
 
 
 if __name__ == '__main__':
