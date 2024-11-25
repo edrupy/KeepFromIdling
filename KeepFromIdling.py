49262959d561ad94561aa85ceadb5cf20662000f
Waterzooi <edrupy@gmail.com>
2024-11-25 14:22:05 +0100

    change to ctrl+alt+F15

--- KeepFromIdling.py
+++ KeepFromIdling.py
@@ -5,18 +5,11 @@ import random
 import pyautogui
 
 def Execute():
-    keys = ['numlock', 'capslock', 'shift']
-    lastUsedKey = None
     while True:
-        availableKeys = [key for key in keys if key != lastUsedKey]
-        key = random.choice(availableKeys)
-        lastUsedKey = key
         waitTime = random.randint(50, 150)
-        print(f'press {key:<8} x2 in 1 sec - sleep {waitTime:<3} sec afterwards')  #:<8 ensures left-aligned within an 8-character wide field
+        print(f'press Ctrl + Alt + F15 - sleep {waitTime:<3} sec afterwards')  #:<3 ensures left-aligned within an 3-character wide field
         time.sleep(1)
-        pyautogui.hotkey(key)
-        time.sleep(0.01)
-        pyautogui.hotkey(key)
+        pyautogui.hotkey('ctrl', 'alt', 'f15')
         time.sleep(waitTime)
 
 
